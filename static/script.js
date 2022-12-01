7d262c1b6b041c0516121947bc2a236fc8050d80
harshalthanki <90134556+harshalthanki@users.noreply.github.com>
2022-12-01 20:45:06 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Fetch the current date and update it in the DOM
+    var date = new Date()
+    let display_date = "Date: " + date.toLocaleDateString()
+
+    $(document).ready(function(){
+        $('#display_date').html(display_date)
+    })
+
+
+
+    //  write an event, when Submit button is clicked
+    $('').click(function(){
+
+        //  get the text value from the textarea using the 'val()' method
+        let text_value = $('').val()
+
+        //  Convert it to JS object.
+        //  Provide a 'key' here and in write the same in app.py file as well to extract data
+        let input_text = {'' : text_value}
+        console.log(input_text)
+
+        //  ajax request
+        $.ajax({
+
+            //  type of web request
+            type : 'POST',
+
+            //  Data to be sent in JSON format
+            data : JSON.stringify(input_data),
+
+            //  type of response expected is json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  if everything is successful, run this function
+            success : function(result){
+
+                // extract prediction and emoticon url from result
+                prediction_review = result.data.prediction_review
+                emo_url = result.data.predicted_review_img_url
+
+                //  update the DOM elements
+                $("#prediction").html(prediction_review)
+                $('#prediction').css("display", "block");
+
+                //  show them
+                $("#emo_img_url").attr('src', emo_url);
+                $('#emo_img_url').css("display", "block");
+            },
+
+            //  if any error, run this function
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  clearing the textbox after every button push
+        $('#text').val("")
+    })
+        
+})
